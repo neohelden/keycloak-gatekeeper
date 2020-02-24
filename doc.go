4145514fd331d6e8186b304f2930ef4c6d30d64a
Maik Hummel <maik@neohelden.com>
2020-02-24 22:08:04 +0100

    Rename requestStateCookie to prevent conflicts

--- doc.go
+++ doc.go
@@ -67,7 +67,7 @@ const (
 	accessCookie       = "kc-access"
 	refreshCookie      = "kc-state"
 	requestURICookie   = "request_uri"
-	requestStateCookie = "OAuth_Token_Request_State"
+	requestStateCookie = "NIAM_OAuth_Token_Request_State"
 	unsecureScheme     = "http"
 	secureScheme       = "https"
 	anyMethod          = "ANY"
